f896669a87d48ce2bd33e6b2dda91f1ef7d5ad8b
Jean Lucas <jeanlsg2001@gmail.com>
2025-11-29 01:05:51 -0300

    feat: Add initial HTML structure and Supabase client setup.

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.https://asksguycdiygwqkzjenn.supabase.co
+const supabaseKey = import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFza3NndXljZGl5Z3dxa3pqZW5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzODYxNjcsImV4cCI6MjA3OTk2MjE2N30.iNOoS-wChmigQmojOEd7y3OE1TApWzQ96LYT1H1tJMU
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
